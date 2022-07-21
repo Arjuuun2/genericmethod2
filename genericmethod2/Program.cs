d14d5b9a7c2eed315f06d646da92bae0e2793c3f
Arjuuun2 <arjunvijay233@gmail.com>
2022-07-21 19:03:48 +0530

    Add project files.

---
+++ genericmethod2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Generic
+{
+    static void Main(String[] ar)
+    {
+        int[] intArray = { 1, 2, 3, 4, 5 };
+        String[] stringArray = { "one", "two", "three", "four" };
+        double[] doubleArray = { 1.0, 2.0, 3.0, 4.0 };
+
+
+        dispalyArrayelements(intArray);
+        dispalyArrayelements(stringArray);
+        dispalyArrayelements(doubleArray);
+    }
+
+
+    public static void dispalyArrayelements<t>(t[] Array)
+    {
+        foreach (t item in Array)
+        {
+            Console.WriteLine(item);
+        }
+        
+        Console.WriteLine();
+
+        
+    }
+}
